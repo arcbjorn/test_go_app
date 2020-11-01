40f8f3042fb3fd7dcfac81568c0f3228534dbc40
arcbjorn <arky.happ@gmail.com>
2020-11-01 19:57:26 +0300

    feat (main): add interfaces

--- Main.go
+++ Main.go
@@ -419,6 +419,44 @@ func main() {
 	}
 	ggg.greet()
 
+	// Interfaces
+	var w Writer = ConsoleWriter{}
+	w.Write([]byte("Hello Go Interface"))
+
+	// Any type interface
+	myInt := IntCounter(0)
+	var inc Incrementer = &myInt
+	for i := 0; i < 10; i++ {
+		fmt.Println(inc.Increment())
+	}
+
+	// empty interface can be any type
+	var emptyInterface interface{}
+	emptyInterface = "emptyInterface"
+	fmt.Println(emptyInterface)
+
+}
+
+type Incrementer interface {
+	Increment() int
+}
+
+type IntCounter int
+
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
+}
+
+type Writer interface {
+	Write([]byte) (int, error)
+}
+
+type ConsoleWriter struct{}
+
+func (cw ConsoleWriter) Write(data []byte) (int, error) {
+	n, err := fmt.Println(string(data))
+	return n, err
 }
 
 type greeter struct {
